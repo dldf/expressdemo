ca12f3f3fc9b218dda9f13c9d5a72c0017693170
Daniel DeFrance <daniel.defrance@montana.edu>
2022-10-26 16:57:06 -0600

    add nodemon

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 5022
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Express!\
+  Good to meet you.')
 })
 
 app.listen(port, () => {
